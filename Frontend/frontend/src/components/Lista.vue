ffbf1789bfd21beb0238eac1cdbc42befa5253e9
Rennan555 <dungeonbr5@gmail.com>
2025-11-12 14:12:38 -0300

    feat: polling auto refresh and initial pagination

--- Frontend/frontend/src/components/Lista.vue
+++ Frontend/frontend/src/components/Lista.vue
@@ -2,17 +2,32 @@
 import axios from 'axios'
 
 export default {
-    data() {
-        return {
-            produtos: [],
-            pagAtual: 1,
-            itensPorPag: 10
-        }
-    },
-    async mounted() {
-        const res = await axios.get('http://localhost:8082/alarm');
-        this.produtos = res.data;
+  data() {
+    return {
+      produtos: [],
+      pagAtual: 1,
+      itensPorPag: 10,
+      numItens: 0
     }
+  },
+  async mounted() {
+    this.getProdutos();
+    this.intervalId = setInterval(() => {
+      this.atualizarPagina();
+    }, 5000) // 5 segundos
+  },
+  methods: {
+    async getProdutos() {
+      const res = await axios.get('http://localhost:8082/alarm');
+      this.produtos = res.data;
+      this.numItens = this.produtos.length
+      this.atualizarPagina();
+    },
+    async atualizarPagina() {
+      if (this.pagAtual > this.produtos.length / this.itensPorPag) this.pagAtual = 1
+      this.produtos = this.produtos.slice((this.pagAtual - 1) * this.itensPorPag, this.pagAtual * this.itensPorPag)
+    }
+  }
 }
 </script>
 
@@ -24,6 +39,8 @@ export default {
         <p>{{ p }}</p>
       </li>
     </ul>
+    <button>+</button>
+    <button>-</button>
   </div>
 </template>
 
